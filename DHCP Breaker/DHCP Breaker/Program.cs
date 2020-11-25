9e93a91e361c7b67a26a2c7e16b94802c4f99250
David Dubey <56110203+20David20@users.noreply.github.com>
2020-11-25 15:55:04 +0100

    Création base du programme
    Ajout d'une fonctionnalité qui permet de couper toutes les cartes réseaux.

---
+++ DHCP Breaker/DHCP Breaker/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DHCP_Breaker
+{
+    class Program
+    {
+
+        static int askAction()
+        {
+            string response;
+            Console.WriteLine("\nVoulez-vous desactiver vos carte reseau? [O/N] ");
+            response = Console.ReadLine();
+            Console.WriteLine(response);
+            if (response == "o" || response == "O")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void desactivateNetwork()
+        {
+            //system("wmic path win32_networkadapter where \"NetEnabled='TRUE'\" call disable");
+            Process.Start("cmd", "/K wmic path win32_networkadapter where \"NetEnabled='TRUE'\" call disable"); // Start a new command prompt, execute command, but leave it open at the end.
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int decision = 0;
+
+            decision = askAction();
+            switch (decision)
+            {
+                case 1:
+                    desactivateNetwork();
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
