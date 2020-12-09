2a2e431cf5a4c1187eb623498f28f3506a7389e0
David Dubey <56110203+20David20@users.noreply.github.com>
2020-12-09 16:11:43 +0100

    Création d'une version formulaire du programme
    En cours de développement

---
+++ DHCP Breaker/DHCP Breaker app/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DHCP_Breaker_app
+{
+    partial class frmDHCPBreaker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbNet = new System.Windows.Forms.ComboBox();
+            this.lblNet = new System.Windows.Forms.Label();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.lstServeurDHCP = new System.Windows.Forms.ListBox();
+            this.lblServeurDHCP = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbNet
+            // 
+            this.cmbNet.FormattingEnabled = true;
+            this.cmbNet.Location = new System.Drawing.Point(12, 267);
+            this.cmbNet.Name = "cmbNet";
+            this.cmbNet.Size = new System.Drawing.Size(355, 23);
+            this.cmbNet.TabIndex = 0;
+            this.cmbNet.SelectedIndexChanged += new System.EventHandler(this.cmbNet_SelectedIndexChanged);
+            // 
+            // lblNet
+            // 
+            this.lblNet.AutoSize = true;
+            this.lblNet.Location = new System.Drawing.Point(12, 249);
+            this.lblNet.Name = "lblNet";
+            this.lblNet.Size = new System.Drawing.Size(180, 15);
+            this.lblNet.TabIndex = 1;
+            this.lblNet.Text = "Choissez la carte réseau à couvrir";
+            this.lblNet.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Location = new System.Drawing.Point(404, 267);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(107, 23);
+            this.cmdStart.TabIndex = 3;
+            this.cmdStart.Text = "Start";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // lstServeurDHCP
+            // 
+            this.lstServeurDHCP.FormattingEnabled = true;
+            this.lstServeurDHCP.ItemHeight = 15;
+            this.lstServeurDHCP.Location = new System.Drawing.Point(12, 67);
+            this.lstServeurDHCP.Name = "lstServeurDHCP";
+            this.lstServeurDHCP.Size = new System.Drawing.Size(136, 154);
+            this.lstServeurDHCP.TabIndex = 4;
+            // 
+            // lblServeurDHCP
+            // 
+            this.lblServeurDHCP.AutoSize = true;
+            this.lblServeurDHCP.Location = new System.Drawing.Point(12, 49);
+            this.lblServeurDHCP.Name = "lblServeurDHCP";
+            this.lblServeurDHCP.Size = new System.Drawing.Size(136, 15);
+            this.lblServeurDHCP.TabIndex = 1;
+            this.lblServeurDHCP.Text = "Serveurs DHCP autorisés";
+            this.lblServeurDHCP.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 436);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "***";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // frmDHCPBreaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 485);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblServeurDHCP);
+            this.Controls.Add(this.lstServeurDHCP);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.lblNet);
+            this.Controls.Add(this.cmbNet);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmDHCPBreaker";
+            this.Text = "DHCP Breaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbNet;
+        private System.Windows.Forms.Label lblNet;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.ListBox lstServeurDHCP;
+        private System.Windows.Forms.Label lblServeurDHCP;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DHCP Breaker/DHCP Breaker app/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using System.Threading;
+
+namespace DHCP_Breaker_app
+{
+    public partial class frmDHCPBreaker : Form
+    {
+
+        private ICaptureDevice device = null;
+        private int choice = 0;
+
+        public frmDHCPBreaker()
+        {
+            InitializeComponent();
+            InitializeSoft();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeSoft()
+        {
+
+            lstServeurDHCP.Items.Add("10.229.60.22");
+            // Print SharpPcap version
+            
+            
+            string ver = SharpPcap.Version.VersionString;
+            //Console.WriteLine("SharpPcap {0}, Example12.PacketManipulation.cs", ver);
+            //Console.WriteLine();
+
+            // Retrieve the device list
+            var devices = CaptureDeviceList.Instance;
+
+            // If no devices were found print an error
+            if (devices.Count < 1)
+            {
+                //Console.WriteLine("No devices were found on this machine");
+                return;
+            }
+
+            int i = 0;
+
+            // Print out the available devices
+            foreach (var dev in devices)
+            {
+                cmbNet.Items.Add(dev.Description);
+            }
+
+            
+            device = devices[choice];
+
+
+            //Register our handler function to the 'packet arrival' event
+            
+
+            // Open the device for capturing
+            //device.Open();
+
+            //Console.WriteLine();
+            //Console.WriteLine("-- Listening on {0}, hit 'Ctrl-C' to exit...",device.Description);
+
+            // Start capture 'INFINTE' number of packets
+            //device.Capture();
+
+            // Close the pcap device
+            // (Note: this line will never be called since
+            //  we're capturing infinite number of packets
+            //device.Close();
+        }
+
+        private void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            if (packet is PacketDotNet.EthernetPacket)
+            {
+
+
+                var ip = packet.Extract<PacketDotNet.IPPacket>();
+                if (ip != null)
+                {
+                    Console.WriteLine("Original IP packet: " + ip.ToString());
+
+                    //manipulate IP parameters
+                    /*ip.SourceAddress = System.Net.IPAddress.Parse("1.2.3.4");
+                    ip.DestinationAddress = System.Net.IPAddress.Parse("44.33.22.11");
+                    ip.TimeToLive = 11;*/
+
+
+
+                    var udp = packet.Extract<PacketDotNet.UdpPacket>();
+                    if (udp != null)
+                    {
+                        MessageBox.Show("Original UDP packet: " + udp.ToString());
+
+                        //manipulate UDP parameters
+
+                        //DETECTION PACKET DHCP
+                        if (udp.ToString().Contains("DHCPMsgType: DHCP Message Type: Ack"))
+                        {
+
+                            if (udp.ToString().Contains("DHCPServerId: Server Id: 10.229.60.22"))
+                            {
+                                //Console.WriteLine();
+                                //Console.WriteLine();
+                                label1.Text = "C'EST OK C'EST OK C'EST OK C'EST OK C'EST OK";
+                                //Console.WriteLine();
+                                //Console.WriteLine();
+
+
+                              
+                            }
+                            else
+                            {
+                                //Console.WriteLine();
+                                //Console.WriteLine();
+                                label1.Text = "HAHAHAAHAAHAHAHAHAHAHHAHHHAHAHAHAHAHAHAH";
+                                //Console.WriteLine();
+                                //Console.WriteLine();
+                                desactivateNetwork();
+                            }
+
+
+
+                            Thread.Sleep(4000);
+
+                            Console.WriteLine(udp.ToString());
+
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        static void desactivateNetwork()
+        {
+            Process.Start("cmd", "/K wmic path win32_networkadapter where \"NetEnabled='TRUE'\" call disable"); // Start a new command prompt, execute command, but leave it open at the end.
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbNet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            label1.Text = cmbNet.SelectedItem.ToString();
+            choice = cmbNet.SelectedIndex;
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+        }
+    }
+}
